3f255defd564778208473dd0369993ee62d7297e
Antonino Cacciottoli <antonino.cacciottoli@outlook.it>
2024-01-06 16:36:44 +0100

    Update lista_libri.c

--- lista_libri.c
+++ lista_libri.c
@@ -4,116 +4,112 @@
 #define N 50
 
 typedef struct {
-	char id[40];
-  	char titolo[150];
-	char genere[40];  
-} libro;
+    char id[40];
+    char title[150];
+    char genre[40];  
+} book;
 
-
-libro archivio_libri[N]={ 
-	{"32 - 45435 - 1223- 4","Il nome della rosa", "romanzo"},
-	{"78- 674 - 5435 - 1223- 4","Il linguaggio C", "informatica"},
-	{"6- 7887- 6 - 8435 - 12234","Il linguaggio C++", "informatica"},
-	{"324- 5- 435 - 1234","Java", "informatica"},
+book library[N] = { 
+    {"32-45435-1223-4", "The Name of the Rose", "novel"},
+    {"78-674-5435-1223-4", "The C Programming Language", "computer science"},
+    {"6-7887-6-8435-12234", "The C++ Programming Language", "computer science"},
+    {"324-5-435-1234", "Java", "computer science"},
 };
 
+int my_index = 4; // Indicates the number of elements allocated in the library array
 
-int my_index=4;
-//indico il numero di elementi allocati nel vettore archivio_libri 
-
-void stampaLibri(int pos)
+void printBooks(int position)
 { 
-int i=0;
-while(i<pos)
-{	
- 	printf("\n\nCodice libro: %s", archivio_libri[i].id);
- 	printf("\nTitolo %s ", archivio_libri[i].titolo);
-	printf("\nGenere %s: ", archivio_libri[i].genere);
+    int i = 0;
+    while(i < position)
+    {	
+        printf("\n\nBook Code: %s", library[i].id);
+        printf("\nTitle: %s", library[i].title);
+        printf("\nGenre: %s", library[i].genre);
 
-	i++;
- }
-}
-
-int insertLibri(int pos)
-{
-	char invio;
-	char c;
-	
-	if(pos>=N) {
-		printf("\n\nHai raggiunto il limite massimo di libri che puoi inserire ");
-		
-	scanf("%c", &invio);
-	return(pos);
-	}
-	
-	fflush(stdin);
-	printf("\nISBN:");
-	gets(archivio_libri[pos].id);
-	fflush(stdin);	
-	printf("\nTitolo:");
-	gets(archivio_libri[pos].titolo);
-	printf("\nGenere:");
-	gets(archivio_libri[pos].genere);
-	fflush(stdin);
-	
-	pos++;
-	return(pos);
-}
-
-void stampaGenere(int pos)
-{ 
-	char genere[40];
-	int i=0, trovato=0;
-	
-	printf("\nQuale genere cerchi?:");
- 	scanf("%s", genere);
- 	
-	for (i=0; i<pos; i++){
-	 	if(strcmp(archivio_libri[i].genere,genere)==0) {
-	 		printf("\n\n ISBN: %s ", archivio_libri[i].id);
-	 		printf("\n Titolo: %s ", archivio_libri[i].titolo);
-	 		trovato=1;
-		 }
- 	}
- 	if (trovato==0) 
-		 printf("\nNon e' presente nessuna categoria:");
-}
-
-int menu_scelta(void)
-{
-  int selezione = 0;
-  do
-    {
-    printf("\n" );
-    printf("\n1 - Stampa libri presenti" );
-    printf("\n2 - Inserisci libro");
-    printf("\n3 - Ricerca libro per genere");
-    printf("\n4 - Esci");
-    printf("\n" );
-    printf("\nEffettua una scelta: " );
-    scanf("%d", &selezione );
+        i++;
     }
-    while (selezione < 1 || selezione > 4);
-  return selezione;
+}
+
+int insertBook(int position)
+{
+    char enter;
+    
+    if(position >= N) {
+        printf("\n\nYou have reached the maximum limit of books you can insert.");
+        
+        scanf("%c", &enter);
+        return position;
+    }
+    
+    fflush(stdin);
+    printf("\nISBN:");
+    gets(library[position].id);
+    fflush(stdin);	
+    printf("\nTitle:");
+    gets(library[position].title);
+    printf("\nGenre:");
+    gets(library[position].genre);
+    fflush(stdin);
+    
+    position++;
+    return position;
+}
+
+void printGenre(int position)
+{ 
+    char genre[40];
+    int i = 0, found = 0;
+    
+    printf("\nWhich genre are you looking for?:");
+    scanf("%s", genre);
+    
+    for (i = 0; i < position; i++){
+        if(strcmp(library[i].genre, genre) == 0) {
+            printf("\n\n ISBN: %s ", library[i].id);
+            printf("\n Title: %s ", library[i].title);
+            found = 1;
+        }
+    }
+    if (found == 0) 
+        printf("\nNo books in this category.");
+}
+
+int menuChoice(void)
+{
+    int selection = 0;
+    do
+    {
+        printf("\n" );
+        printf("\n1 - Print available books" );
+        printf("\n2 - Insert a book");
+        printf("\n3 - Search for books by genre");
+        printf("\n4 - Exit");
+        printf("\n" );
+        printf("\nMake a choice: " );
+        scanf("%d", &selection );
+    }
+    while (selection < 1 || selection > 4);
+    return selection;
 }
 
 int main(void)
 {
-    int scelta;
+    int choice;
     
-    while((scelta=menu_scelta())!=4){
-    switch(scelta){
-        case 1: 
-               stampaLibri(my_index);
-               break;
-        case 2:
-               my_index=insertLibri(my_index);
-               break;
-        case 3:
-               stampaGenere(my_index);
-               break;
-        case 4: break;
-             }
+    while((choice = menuChoice()) != 4) {
+        switch(choice) {
+            case 1: 
+                printBooks(my_index);
+                break;
+            case 2:
+                my_index = insertBook(my_index);
+                break;
+            case 3:
+                printGenre(my_index);
+                break;
+            case 4: break;
+        }
     }             
     return 0;
 }
